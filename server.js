f75b2e107100f6dc9659cc5b8a919871fae60a46
GOYAT-UI <your@email.com>
2026-05-18 18:04:07 +0530

    add cors

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const crypto = require('crypto');
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3001;
